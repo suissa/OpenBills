3ae2621a54d95bd3fcfc82b4577efb273a2b070f
Suissa <jnascimento@gmail.com>
2017-04-02 16:48:59 -0300

    agora popula soh os campos q vc passar na query como entities, ex: /populate/bills

--- api/_organelles/findAllPopulate.js
+++ api/_organelles/findAllPopulate.js
@@ -3,13 +3,17 @@ const theseError = require('./ribosomes/error-json')
 const filterToPopulate = require('./helpers/filterToPopulate')
 const filterToPopulateArray = require('./helpers/filterToPopulateArray')
 
+const removeStringIfHas = ( something ) => ( str ) =>
+  !str.includes( something )
+
+const removeFieldWithoutRef = ( paths ) => ( field ) => 
+  paths[ field ].instance == 'ObjectID' ||
+  paths[ field ].instance == 'Array'
 
 const getFields = ( paths ) => 
   Object.keys( paths )
-        .filter( field => !field.includes('_') )
-        .filter( field => 
-          paths[ field ].instance == 'ObjectID' ||
-          paths[ field ].instance == 'Array' )
+        .filter( removeIfStringHas( '_' ) )
+        .filter( removeFieldWithoutRef( paths ) )
 
 const reduceFieldsToPopulate = ( fields ) =>
   fields.reduce( filterToPopulateArray, [] )
@@ -17,14 +21,27 @@ const reduceFieldsToPopulate = ( fields ) =>
 const reduceFieldsToPopulateArray = ( fields ) =>
   fields.reduce( filterToPopulate )
 
-const areToPopulate = ( fields ) =>
-  ( fields.length > 1 )
-    ? reduceFieldsToPopulate( fields )
-    : reduceFieldsToPopulateArray( fields )
+const areToPopulate = ( req, fields ) =>
+  ( req.query.entities )
+    ? req.query
+          .entities
+          .split(',')
+          .filter( filterToPopulate( req ) )
+    : ( fields.length > 1 )
+        ? reduceFieldsToPopulate( fields )
+        : reduceFieldsToPopulateArray( fields )
+
+
+    // const fieldsToPopulate = (req.query.entities)
+    //                             ? req.query
+    //                                 .entities
+    //                                 .split(',')
+    //                                 .filter(filterToPopulate)
+    //                             : '' //depois pegar automatico
 
 module.exports = (Organism) => 
   (req, res) => {
-    const thisFields = areToPopulate( getFields( Organism.schema.paths ) )
+    const thisFields = areToPopulate( req, getFields( Organism.schema.paths ) )
 
     return Organism.findOne( {} )
                     .populate( thisFields )
